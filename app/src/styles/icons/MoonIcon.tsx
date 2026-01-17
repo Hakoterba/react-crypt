e9861085b8951c74eac861f4e37f5048d5736f68
hakoterba <55061454+Hakoterba@users.noreply.github.com>
2026-01-17 16:14:03 +0100

    ajout d'icon

---
+++ app/src/styles/icons/MoonIcon.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from 'react';
+
+export const MoonIcon = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        {...props}
+    >
+        <path d="M21 12.79A9 9 0 0111.21 3 7 7 0 1012 21a9 9 0 009-8.21z" />
+    </svg>
+);
