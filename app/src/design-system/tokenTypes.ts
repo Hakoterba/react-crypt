887f7eaea99c54d74364b1c1dda8cd14fefe2e6d
hakoterba <55061454+Hakoterba@users.noreply.github.com>
2026-01-19 11:34:27 +0100

    tokens css supprimés

--- app/src/design-system/tokenTypes.ts
+++
@@ -1,8 +0,0 @@
-import type { tokenVariables } from './tokenVariables';
-
-export type ColorType =
-    | keyof (typeof tokenVariables)['color']['primary']
-    | keyof (typeof tokenVariables)['color']['secondary']
-    | keyof (typeof tokenVariables)['color']['greyscale'];
-
-export type SpacingType = keyof (typeof tokenVariables)['spacing'];
--- app/src/design-system/tokenVariables.ts
+++
@@ -1,7 +0,0 @@
-import color from './color.tokens.json';
-import spacing from './spacing.tokens.json';
-
-export const tokenVariables = {
-    ...color,
-    ...spacing,
-} as const;
