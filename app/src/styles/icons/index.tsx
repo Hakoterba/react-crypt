616009e936f576c7a5c16a2ecf9d269f13e5d1dd
hakoterba <55061454+Hakoterba@users.noreply.github.com>
2026-01-19 11:35:41 +0100

    add icon

--- app/src/styles/icons/index.tsx
+++ app/src/styles/icons/index.tsx
@@ -2,3 +2,4 @@ export { SunIcon } from './SunIcon';
 export { MoonIcon } from './MoonIcon';
 export { WalletIcon } from './WalletIcon';
 export { AddIcon } from './AddIcon';
+export { BitcoinIcon } from './BitcoinIcon';
